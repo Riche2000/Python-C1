a6897ca2e7545832e7de2b693e324e9862982b86
Riche2000 <ricardosanch2000@gmail.com>
2023-03-02 10:04:28 -0600

    Introducir datos y mostrar

--- PrimerPrograma/primerprograma.py
+++ PrimerPrograma/primerprograma.py
@@ -2,5 +2,12 @@ a = int(input("Digita un número: "))
 b = int(input("Digita un segundo número: "))
 c = int(input("Digita un tercer número: "))
 
-print("El número más grande es: ",max(a,b,c))
-print("El número más pequelo es: ",min(a,b,c))
+#Para pode imprimir en patnalla un string y un numero seria:
+# "Tu numero es : {}".format(5)
+
+#print("El número más grande entre {}, {} y {} es: {}".format(a, b, c, max(a,b,c)))
+#print("El número más pequelo entre {}, {} y {} es: {}".format(a, b, c, min(a,b,c)))
+
+print("El número más grande entre {}, {} y {} es {} y el más pequeño es {}".format(a, b, c,
+                                                                                max(a,b,c),
+                                                                                min(a,b,c)))
