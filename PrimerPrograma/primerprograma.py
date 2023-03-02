6b8ab00aa70ee6d90deb915478dca77901e328f3
Riche2000 <ricardosanch2000@gmail.com>
2023-03-01 22:34:37 -0600

    Ingresar datos y mostrar en pantalla

---
+++ PrimerPrograma/primerprograma.py
@@ -0,0 +1,6 @@
+a = int(input("Digita un número: "))
+b = int(input("Digita un segundo número: "))
+c = int(input("Digita un tercer número: "))
+
+print("El número más grande es: ",max(a,b,c))
+print("El número más pequelo es: ",min(a,b,c))
