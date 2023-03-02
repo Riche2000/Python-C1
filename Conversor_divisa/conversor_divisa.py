60e2cbc088f8129fbb6862f0ee7e804307fd45a4
Riche2000 <ricardosanch2000@gmail.com>
2023-03-02 10:22:04 -0600

    Conversor de grados

---
+++ Conversor_divisa/conversor_divisa.py
@@ -0,0 +1,4 @@
+numero = int(input("Dime un número"))
+numero = float(input("Dime un número  decimal: "))
+
+print(numero)
---
+++ Conversor_temp/farenheit_celcius.py
@@ -0,0 +1,5 @@
+print("Bienvenido a mi conversor de Farenheit a Celcius")
+grados_farenheit = int(input("Grados Farenheit: "))
+grados_celcius = (grados_farenheit-32)*5/9
+
+print("El resultado es: ",format(grados_celcius))
