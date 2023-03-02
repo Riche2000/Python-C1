117eb1a71e3356228994b4f07a28e0342e08752b
Riche2000 <ricardosanch2000@gmail.com>
2023-03-02 10:28:41 -0600

    Conversor de dolares a pesos

--- Conversor_divisa/conversor_divisa.py
+++ Conversor_divisa/conversor_divisa.py
@@ -1,4 +1,3 @@
-numero = int(input("Dime un número"))
-numero = float(input("Dime un número  decimal: "))
-
-print(numero)
+cantidad_en_dolares = float(input("Digite la cantidad de dolares: "))
+conversion_dolar_peso = float(input("Introduzca la conversion peso a dolar: "))
+print("La cantidad de dolares es: {}".format(cantidad_en_dolares*conversion_dolar_peso))
