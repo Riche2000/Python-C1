b3823208e96c1f425ef92a824218f6a5ebbd8548
Riche2000 <ricardosanch2000@gmail.com>
2023-03-06 12:50:01 -0600

    Comer objetos

--- Laberinto/maze.py
+++ Laberinto/maze.py
@@ -13,6 +13,8 @@ MAP_HEIGHT = 15
 #              x, y
 my_position = [6,3]
 
+map_objects = [[2,3],[5,4],[3,4],[10,6]]
+
 while True:
 
     #Draw Map
@@ -21,10 +23,20 @@ while True:
         print("|",end="")
         #A cada una de las iteraciones que tenga en mi for voy a tern un número desde el 0 hasta 19
         for coordinate_x in range(MAP_WIDTH):
+            char_to_draw = " "
+            object_in_cell = None
+            for map_object in map_objects:
+                if map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
+                    char_to_draw = "*"
+                    object_in_cell = map_object
             if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
-                print(" @ ",end="")
-            else:
-                print("   ",end="")
+                char_to_draw = "@"
+
+                if object_in_cell:
+                    map_objects.remove(object_in_cell)
+
+
+            print(" {} ".format(char_to_draw), end="")
         print("|")
 
     print("+"+"-"*MAP_WIDTH*3+"+")
