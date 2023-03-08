08d2bab481e20a0e3e96d1ceb2118b2c653a3776
Riche2000 <ricardosanch2000@gmail.com>
2023-03-08 11:07:20 -0600

    Funciones

--- Funciones/funciones.py
+++ Funciones/funciones.py
@@ -12,3 +12,20 @@ def saludo_sectario(nombre):
 saludo_sectario("Ricardo")
 saludo_sectario("Alfredo Solano")
 saludo_sectario("Sergio Hernandez")
+
+def largo_string(mi_string):
+    largo = 0
+    for n in mi_string:
+        largo += 1
+    return largo
+
+largo_de_la_string = largo_string("Hola mundo")
+
+print(largo_de_la_string)
+
+def main():
+    print("Hola mundo")
+
+#Ejuectuamos la funcion para importar se usa __funcion__
+if __name__ == "__main__":
+    main()
