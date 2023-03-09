bf18c70c74608bfb5066a1b91ddd2dd94c49ef06
Riche2000 <ricardosanch2000@gmail.com>
2023-03-09 09:58:52 -0600

    Funciones recursivas

--- Funciones/main.py
+++ Funciones/main.py
@@ -2,6 +2,59 @@
 from time import sleep
 
 #Funciones recursivas
+def fibonacci_recursivo(n):
+    if n <= 1:
+        return 1
+    return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
+
+def medir_largos(iterable, *args, sumar_todo=False):
+    if args:
+        largos = [len(iterable)]
+        for a in args:
+            largos.append(len(a))
+        if sumar_todo:
+            largos = sum(largos)
+        return largos
+    return len(iterable)
+
+def potencia(numero,base=2):
+    resultado = numero
+    for a in range(1, base):
+        resultado *= numero
+    return resultado
+
+def main():
+        print(potencia(4))
+        print(potencia(4,5))
+
+if __name__ == "__main__":
+    main()
+
+"""
+def fibonacci_recursivo(n):
+    if n <= 1:
+        return 1
+    return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
+
+def medir_largos(iterable, *args, sumar_todo=False):
+    if args:
+        largos = [len(iterable)]
+        for a in args:
+            largos.append(len(a))
+        if sumar_todo:
+            largos = sum(largos)
+        return largos
+    return len(iterable)
+
+def main():
+    for a in range(10):
+        print(fibonacci_recursivo(a))
+
+if __name__ == "__main__":
+    main()
+"""
+
+"""
 def medir_largos(iterable,*args,sumar_todo=False):
     if args:
         largos = [len(iterable)]
@@ -18,7 +71,7 @@ def main():
 
 if __name__ == "__main__":
     main()
-
+"""
 
 """
 def medir_largos(iterable, *args, sumar_todo=False):
